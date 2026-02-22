ade68c5fb09f9bb5b0baadd1337f80a64ab7ab3d
eventflowapp2026-ux <eventflowapp.2026@gmail.com>
2026-02-22 21:34:14 +0530

    Add PYTHON_VERSION to build.sh to force Python 3.11

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # Exit on error
 set -o errexit
 
+# Force Python 3.11 (this is the key line that was missing!)
+export PYTHON_VERSION=3.11.9
+
 # Install dependencies
 pip install -r requirements.txt
 
@@ -10,3 +13,6 @@ python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --noinput
+
+# Optional: Show Python version to verify
+python --version
