76fa156fbcaec3b2ed7a9361ac66cb2d5bae5590
eventflowapp2026-ux <eventflowapp.2026@gmail.com>
2026-02-25 10:43:00 +0530

    Fix build.sh syntax error

--- build.sh
+++ build.sh
@@ -9,21 +9,4 @@ python manage.py collectstatic --noinput
 # Create superuser from environment variables
 python community/ensure_superuser.py
 
-python --version#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Force Python 3.11 (this is the key line that was missing!)
-export PYTHON_VERSION=3.11.9
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Run migrations
-python manage.py migrate
-
-# Collect static files
-python manage.py collectstatic --noinput
-
-# Optional: Show Python version to verify
 python --version
